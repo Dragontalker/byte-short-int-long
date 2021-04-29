89c75b0adcd50dfde6444ee4fe7eea4e0cff6008
Richard <richard.yang.tong@gmail.com>
2021-04-28 20:15:58 -0400

    Added range of byte.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -13,5 +13,10 @@ public class Main {
 
         int myMaxIntTest = 2_147_483_647;
         System.out.println(myMaxIntTest);
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
     }
 }
