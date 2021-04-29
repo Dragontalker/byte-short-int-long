80b0e59a7f4297c5e0527d67060f7b880afcddba
Richard <richard.yang.tong@gmail.com>
2021-04-28 20:34:13 -0400

    Added an example for casting.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -31,5 +31,13 @@ public class Main {
         System.out.println("Long Maximum Value = " + myMaxLongValue);
         long bigLongLiteralValue = 2_147_483_647_284L;
         System.out.println(bigLongLiteralValue);
+
+        int myTotal = (myMinIntValue /2 );
+
+        byte myNewByteValue = (byte) (myMinIntValue / 2);
+        System.out.println("The casting byte value is " + myNewByteValue);
+
+        short myNewShortValue = (short) (myMinIntValue / 2);
+        System.out.println("The casting short value is " + myNewShortValue);
     }
 }
