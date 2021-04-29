aacab26b1dff05bb78a465ad01a4a95683c3cfb1
Richard <richard.yang.tong@gmail.com>
2021-04-28 20:07:39 -0400

    Inspected the maximum and minimum value of initeger type.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,6 +3,10 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
     }
 }
